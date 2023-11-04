0aa5091ae4723246ddb6fd2c4cd61207d1e3070d
omegaee <2955281552@qq.com>
2023-11-04 13:22:17 +0800

    update readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - 可配置模块 - 友好的配置界面
 - 支持大多数版本（包括后期更新的版本）
-- 依赖 LSPosed / Xposed / EdXposed
+- 依赖 LSPosed / Xposed / EdXposed / VirtualXposed
 
 ### 功能
 
@@ -10,8 +10,17 @@
 - [x] 修改游戏设备ID -- 自由切换设备
 - [x] 广告10秒 -- 仅限TapTap版
 
+### 测试环境
+
+- 雷电模拟器9 + LSPosed + PVZ2-TapTap-v3.1.5
+
 ### 附
 
 - [Magisk - Github](https://github.com/topjohnwu/Magisk)
 - [Magisk Delta - Github](https://github.com/HuskyDG/magisk-files)
 - [LSPosed - Github](https://github.com/LSPosed/LSPosed)
+- [VirtualXposed - Github](https://github.com/android-hacker/VirtualXposed)
+
+### 免责声明
+
+> 本项目是一个开源的非商业性质的作品，同时是一个游戏插件，不属于游戏官方的一部分，也不代表游戏官方的观点或立场。遵循`MIT许可证`的规定。本项目的源代码和资源可以在`Github`上免费获取和修改，但不得用于任何商业目的。本项目的作者和贡献者不对本项目的质量和安全性作出任何保证，也不保证本项目与游戏的兼容性和稳定性。本项目的使用者应该自行判断本项目是否适合自己的需求，以及是否存在任何风险或隐患。本项目的作者和贡献者不对本项目的使用或滥用承担任何责任，也不对本项目可能造成的任何损害或不利后果负责。本项目的使用者应该遵守游戏的规则和条款，以及相关的法律法规，否则后果自负。
--- app/src/main/java/cn/omegaee/pvz2plugin/MainActivity.java
+++ app/src/main/java/cn/omegaee/pvz2plugin/MainActivity.java
@@ -1,21 +1,41 @@
 package cn.omegaee.pvz2plugin;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected TextView tip;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
+        initView();
+
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.content, new ConfigModule())
                 .commit();
+
+    }
+
+    private void initView(){
+        tip = findViewById(R.id.tip);
+        if(tip != null){
+            String html = "本软件完全免费 --- " +
+                    "<a href=\"https://github.com/omegaee/pvz2-plugin\">官网 - github</a>";
+            Spanned spanned = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT);
+            tip.setMovementMethod(LinkMovementMethod.getInstance());
+            tip.setText(spanned);
+        }
 
     }
 }
